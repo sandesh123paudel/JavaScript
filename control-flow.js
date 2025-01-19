5160aae52b4f039edba44e0b73ca9d480d55bed9
Sandesh Paudel <sandeshpaudel0618@gmail.com>
2025-01-19 17:21:32 +0545

    exercise done

--- control-flow.js
+++ control-flow.js
@@ -29,20 +29,129 @@
 
 //SWITCH CASE STATEMENT
 
-let job='teacher';
+// let job='teacher';
 
-switch(job){
-    case 'teacher':
-        console.log('Teaches kids');
-        break;
-    case 'driver':
-        console.log('Drives a cab');
-        break;
-    case 'cop':
-        console.log('Fights crime');
+// switch(job){
+//     case 'teacher':
+//         console.log('Teaches kids');
+//         break;
+//     case 'driver':
+//         console.log('Drives a cab');
+//         break;
+//     case 'cop':
+//         console.log('Fights crime');
     
-    default:
-        console.log('Does something else');
-}
+//     default:
+//         console.log('Does something else');
+// }
 
+
+
+// let numbers = [1,2,3,4,5,6,7];
+
+
+
+// for (let idx=0; idx<numbers.length; idx++){
+//     console.log(numbers[idx]);
+// }
+
+// for (let num=0;num<=100;num++){
+//     console.log(num);
+// }
+
+
+//////WHILE LOOP
+// let idx=0
+// while(idx<numbers.length){
+//     console.log(numbers[idx]);
+//     idx++;
+// }
+
+
+// let sum=0;
+
+// while(true){
+//     console.log('Loop');
+//     sum++;
+
+//     if(sum===10)
+//         break;
+// }
+
+
+//////DO WHILE LOOP
+
+
+// let idx=0;
+// do{
+//     console.log(numbers[idx]);
+//     idx++;
+// } while(idx<numbers.length);
+
+
+
+//// FOR IN LOOP
+
+// cont=course={
+//     name:'Javascript',
+//     duration:2,
+//     sections:7
+// };
+
+// console.log(course.name);
+// console.log(course['duration']);
+// console.log(course.sections);
+
+// for( const key in course){
+//     console.log(course[key]);
+// }
+
+
+
+
+//// FOR of LOOP
+
+// for(let idx=0;idx<numbers.length;idx++){
+//     console.log(numbers[idx]);
+// }
+
+
+// for (let element of numbers){
+//     console.log(element);
+// }
+
+
+
+/// Break and Continue
+
+// for (let i=0;i<10;i++)
+// {
+//     if(i===5){
+//         break;
+
+//     }
+//     console.log(i);
+// }
+
+// let i=0;
+// while(i<10){
+//     if(i===5){
+//         break;
+//     }
+//     console.log(i);
+//     i++;
+
+// }
+
+
+let i=0;
+do{
+    if(i===5){
+        break;
+    }
+    console.log(i);
+    i++
+}while(i<10);
+
+let a=0;
 
---
+++ exercise1.js
@@ -0,0 +1,51 @@
+// function max(num1,num2){
+//     // if(num1>num2)
+//     //     return num1
+//     // else
+//     //  return num2
+//     return num1>=num2 ?num1 :num2;
+// }
+
+
+// console.log(max(12,12))
+
+
+
+// function hello(num){
+//     if(num%3==0 && num%5==0)
+//         return "FizzBuzz";
+//     else if(num%3==0)
+//         return "Fizz";
+//     else if(num%5==0)
+//         return "Buzz";
+//     else
+//         return num;
+// }
+
+// console.log(hello(8))
+
+
+//Even Number
+// function arr(a){
+//     for(const i of a)
+//     {
+//         if(i%2===0)
+//         {
+//             console.log(i)
+//         }
+//     }
+// }
+
+//Odd Number
+function arr(a){
+    for(const i of a)
+    {
+        if(i%2 !=0)
+        {
+            console.log(i)
+        }
+    }
+}
+
+arr([1,2,3,4,5])
+
