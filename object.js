207865c8ae2e540266869f76831b16137ec0dbaa
Sandesh Paudel <sandeshpaudel0618@gmail.com>
2025-01-23 11:24:45 +0545

    Accepting user input

--- object.js
+++ object.js
@@ -1,30 +1,49 @@
-const dog={
-    name:'Max',
-    breed:'Dachshund',
-    age:5,
-    weightInPounds:12,
-    eat:function(){
-        console.log('Champ!');
-    },
-    bark(){
-        console.log('Woof!');
-    }
+// const dog={
+//     name:'Max',
+//     breed:'Dachshund',
+//     age:5,
+//     weightInPounds:12,
+//     eat:function(){
+//         console.log('Champ!');
+//     },
+//     bark(){
+//         console.log('Woof!');
+//     }
+// }
+
+// function getDog(name,breed,age,weightInPounds){
+//     return{
+//         name,
+//         breed,
+//         age,
+//         weightInPounds,
+//         eat(){
+//             console.log('Champ!');
+//         },
+//         bark(){
+//             console.log('Woof!');
+//         }
+//     }
+// }
+
+// const anotherDog=getDog('nancy','jack',2,50)
+// console.log(anotherDog);
+
+
+
+///Objects are dynamic
+
+const person ={
+    name:'Sandesh'
+};
+
+person.favoriteFood='tacos';
+console.log(person);
+
+
+
+person.eat=function(){
+    console.log('Start Eating');
 }
 
-function getDog(name,breed,age,weightInPounds){
-    return{
-        name,
-        breed,
-        age,
-        weightInPounds,
-        eat(){
-            console.log('Champ!');
-        },
-        bark(){
-            console.log('Woof!');
-        }
-    }
-}
-
-const anotherDog=getDog('nancy','jack',2,50)
-console.log(anotherDog);
+console.log(person.eat);
