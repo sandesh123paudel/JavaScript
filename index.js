61336155028c0269f815ef12179c1d50ede9cabf
Sandesh Paudel <sandeshpaudel0618@gmail.com>
2025-01-23 12:58:28 +0545

    Random Number Generator Completed

--- index.js
+++ index.js
@@ -48,9 +48,9 @@
 // console.log(z,typeof(z));
 
 
-const PI=3.1465; /// we can make this const so that we cannot change that further in any line of code
-let radius;
-let circumference;
+// const PI=3.1465; /// we can make this const so that we cannot change that further in any line of code
+// let radius;
+// let circumference;
 
 
 // radius = window.prompt("Enter a radius");
@@ -58,17 +58,67 @@ let circumference;
 
 
 
-document.getElementById('mySubmit').onclick=function(){
-    radius=document.getElementById("myText").value;
-    radius=Number(radius);
+// document.getElementById('mySubmit').onclick=function(){
+//     radius=document.getElementById("myText").value;
+//     radius=Number(radius);
 
-    circumference = 2 * PI * radius;
+//     circumference = 2 * PI * radius;
 
-    document.getElementById('result').textContent= `The Circumference is ${circumference}`;
+//     document.getElementById('result').textContent= `The Circumference is ${circumference}`;
 
 
+// }
+
+
+
+/// Math - Builtin object that provides a collection of properties and methods
+
+
+// let x=82.2332;
+// let y=2;
+// let z;
+
+
+// z=Math.round(x);
+// console.log(z);
+
+// z=Math.pow(x,y);
+// console.log(z);
+
+// z=Math.sqrt(x);
+// console.log(z);
+
+// z=Math.ceil(x);
+// console.log(z);
+
+// z=Math.floor(x);
+// console.log(z);
+
+// z=Math.min(2,3,4,5,6,7,1,0,-1,-2);
+// console.log(z);
+
+// z=Math.max(2,3,4,5,6,7,1,0,-1,-2);
+// console.log(z);
+
+// z=Math.random();
+// console.log(z);
+
+// z=Math.floor(Math.random()*10);
+
+
+// const min=50, max=100;
+// let randomNumber= Math.floor(Math.random()*(max-min))+min;
+// console.log(randomNumber);
+
+
+let randomNumber;
+const myButton=document.getElementById('submit');
+const myLabel=document.getElementById('result')
+const min=1;
+const max=6;
+
+myButton.onclick=function()
+{
+   randomNumber=Math.floor(Math.random()*max+min);
+   myLabel.textContent='Generated Random Number is: '+ randomNumber;
 }
-
-
-
-
