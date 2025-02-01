fa0f091d68ff53f29f3f8c12ca67485d1fdafafc
Sandesh Paudel <sandeshpaudel0618@gmail.com>
2025-02-01 15:22:24 +0545

    Random Password Generator Completed

---
+++ password.js
@@ -0,0 +1,55 @@
+//Random Password Generator
+
+
+
+function generatePassword(length,includeLowerCase,includeUpperCase,includeNumbers,includeSymbols){
+
+    const lowercaseChars="abcdefghijklmnopqrstuvwxyz";
+    const uppercaseChars="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    const numberChars="0123456789";
+    const symbolChars="!@#%$^&*()_+-=";
+
+    let allowedChars="";
+    let password="";
+
+    allowedChars +=includeLowerCase?lowercaseChars:"";
+    allowedChars +=includeUpperCase?uppercaseChars:"";
+    allowedChars +=includeNumbers?numberChars:"";
+    allowedChars +=includeSymbols?symbolChars:"";
+
+
+
+    console.log(allowedChars);
+
+    if(length <=0){
+        return `(password length must be at least 1)`;
+
+    }
+    if(allowedChars.length===0){
+        return `(At least 1 set of characters needs to be selected)`;
+
+    }
+
+    for(let i=0;i<length;i++)
+    {
+        const randomIndex=Math.floor(Math.random()*allowedChars.length);
+        console.log(randomIndex);
+        password += allowedChars[randomIndex];
+    }
+    return password;
+
+}
+const passwordLength=8;
+const includeLowerCase=true;
+const includeUpperCase=true;
+const includeNumbers=true;
+const includeSymbols=true;
+
+
+const password=generatePassword(passwordLength,
+                                includeLowerCase,
+                                includeUpperCase,
+                                includeNumbers,
+                                includeSymbols);
+
+console.log(`Generated Password: ${password}`);
