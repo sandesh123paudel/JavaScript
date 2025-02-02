bc267dcb35ea4ed93d572d70ffeec10e91cde69f
Sandesh Paudel <sandeshpaudel0618@gmail.com>
2025-02-02 17:07:01 +0545

    callback forEach completed

--- callbacks.js
+++ callbacks.js
@@ -34,19 +34,42 @@
 // }
 
 
-sum(1,2,displayPage);
+// sum(1,2,displayPage);
 
-function  sum(x,y,callback){
-    let result = x+y;
-    callback(result);
+// function  sum(x,y,callback){
+//     let result = x+y;
+//     callback(result);
+// }
+
+// function displayConsole(result){
+//     console.log(result);
+// }
+
+
+// function displayPage(result)
+// {
+//     document.getElementById("myH1").textContent=result;
+// }
+
+
+
+///foreach()
+
+/// forEach() = method used to iterate over the elements of an array 
+//              and apply a specified function(callback) 
+//               to each element
+
+//              array.forEach(callback)
+
+let numbers=[1,2,3,4,5];
+
+numbers.forEach(double);
+numbers.forEach(display);
+
+function display(element){
+    console.log(element);
 }
 
-function displayConsole(result){
-    console.log(result);
-}
-
-
-function displayPage(result)
-{
-    document.getElementById("myH1").textContent=result;
+function double(element,index,array){
+    array[index]=element*2;
 }
