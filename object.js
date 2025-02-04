2b85b6a0d20f788c52af96802f3f48aa5e1d0e4d
Sandesh Paudel <sandeshpaudel0618@gmail.com>
2025-02-04 21:58:56 +0545

    this in js

--- object.js
+++ object.js
@@ -105,25 +105,25 @@
 //          can represent real wordl objects(people,products,places) 
 //           object={key:value, function()} 
 
-const person1={
-    firstName:"Sandesh",
-    lastName:"Paudel",
-    age:30,
-    isEmployed:true,
-    sayHello:function(){console.log("Hi, I am Sandesh")},
-    eat: function(){console.log("I am eating a Krabby Patty")}
-};
+// const person1={
+//     firstName:"Sandesh",
+//     lastName:"Paudel",
+//     age:30,
+//     isEmployed:true,
+//     sayHello:function(){console.log("Hi, I am Sandesh")},
+//     eat: function(){console.log("I am eating a Krabby Patty")}
+// };
 
-const person2={
-    firstName:"Patrick",
-    lastName:"Star",
-    age:42,
-    isEmployed:false,
-    sayHello:()=>console.log("Hey, I am Patrick!"),
-    eat:()=>console.log("I am eating a Chicken Pizza")
+// const person2={
+//     firstName:"Patrick",
+//     lastName:"Star",
+//     age:42,
+//     isEmployed:false,
+//     sayHello:()=>console.log("Hey, I am Patrick!"),
+//     eat:()=>console.log("I am eating a Chicken Pizza")
 
 
-};
+// };
 
 // console.log(person1.firstName);
 // console.log(person1.lastName);
@@ -135,8 +135,20 @@ const person2={
 // console.log(person2.age);
 // console.log(person2.isEmployed);
 
-person1.sayHello();
-person2.sayHello();
+// person1.sayHello();
+// person2.sayHello();
 
-person1.eat();
-person2.eat();
+// person1.eat();
+// person2.eat();
+
+
+
+///THIS keyword
+
+const person1={
+    name:"Sandesh",
+    favFood:"hamburgers",
+    sayHello:function(){console.log(`Hi I am ${this.name}`)}
+}
+
+person1.sayHello();
