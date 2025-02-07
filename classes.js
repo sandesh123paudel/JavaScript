d243b7545de72f7c0a6c951a4eb65cd570b010b7
Sandesh Paudel <sandeshpaudel0618@gmail.com>
2025-02-07 21:54:49 +0545

    .

---
+++ classes.js
@@ -0,0 +1,4 @@
+//class-> (ES6 function) provides a more structured and cleaner way to 
+//          work with the objects compared to traitional construstor funcitons 
+//          ex.static keyword,encapsulation,inheritance 
+
