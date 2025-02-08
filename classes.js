3a8033fab68d35c65e17284f9606c173ae81eb39
Sandesh Paudel <sandeshpaudel0618@gmail.com>
2025-02-08 13:10:22 +0545

    Super Keyword Usage Completed

--- classes.js
+++ classes.js
@@ -125,62 +125,130 @@
 //                   helps with code reusabliliy
 
 
-class Animal{
-    alive=true;
+// class Animal{
+//     alive=true;
 
-    eat(){
-       console.log(`This ${this.name} is eating`); 
+//     eat(){
+//        console.log(`This ${this.name} is eating`); 
+//     }
+
+//     sleep(){
+//         console.log(`This ${this.name} is sleeping`); 
+//      }
+// }
+
+
+// class Rabbit extends Animal{
+//      name="Rabbit"; 
+
+//      //can have individual functions too
+//      run(){
+//         console.log(`This ${this.name} is running`);
+//      }
+// }
+
+// class Fish extends Animal{
+//      name="Fish";
+//      swim(){
+//         console.log(`This ${this.name} is swimming`);
+//      }
+// }
+
+// class Hawk extends Animal{
+//      name="Hawk"; 
+
+//      fly(){
+//         console.log(`This ${this.name} is flying`);
+//      }
+// }
+
+// const rabbit= new Rabbit();
+// const fish= new Fish();
+// const hawk= new Hawk();
+
+// rabbit.alive=false;
+// console.log(rabbit.alive);
+// rabbit.eat();
+// rabbit.sleep();
+// rabbit.run();
+
+// console.log(fish.alive);
+// fish.eat();
+// fish.sleep();
+// fish.swim();
+
+// console.log(hawk.alive);
+// hawk.eat();
+// hawk.sleep();
+// hawk.fly();
+
+
+
+
+///SUPER :->  keyword is used in classes to call the constructor or 
+//            access the properties and methods of a parent (superclass) 
+//            this=this.object 
+//            super=the parent
+
+
+class Animal
+{
+    constructor(name, age){
+        this.name=name;
+        this.age=age;
     }
 
-    sleep(){
-        console.log(`This ${this.name} is sleeping`); 
-     }
+    move(speed)
+    {
+        console.log(`The ${this.name} moves at a speed of ${speed}mph`);
+    }
+
 }
 
-
 class Rabbit extends Animal{
-     name="Rabbit"; 
+    constructor(name,age,runSpeed){
 
-     //can have individual functions too
-     run(){
-        console.log(`This ${this.name} is running`);
-     }
+        //super keyword is used to access the parent class properties
+        super(name,age);
+        this.runSpeed=runSpeed;
+    }
+
+    run(){
+        console.log(`This ${this.name} can run`);
+        super.move(this.runSpeed);
+    }
+
 }
 
 class Fish extends Animal{
-     name="Fish";
-     swim(){
-        console.log(`This ${this.name} is swimming`);
-     }
+    constructor(name,age,swimSpeed){
+        super(name,age);
+        this.swimSpeed=swimSpeed;
+    }
+    swim(){
+        console.log(`This ${this.name} can swim`);
+        super.move(this.swimSpeed);
+    }
+    
 }
-
 class Hawk extends Animal{
-     name="Hawk"; 
-
-     fly(){
-        console.log(`This ${this.name} is flying`);
-     }
+    constructor(name,age,flySpeed){
+        super(name,age);
+        this.flySpeed=flySpeed;
+    }
+    fly(){
+        console.log(`This ${this.name} can fly`);
+        super.move(this.flySpeed);
+    }
 }
+    
+const rabbit=new Rabbit("rabbit",1,25);
+const fish=new Fish("fish",2,12);
+const hawk=new Hawk("hawk",3,50);
 
-const rabbit= new Rabbit();
-const fish= new Fish();
-const hawk= new Hawk();
 
-rabbit.alive=false;
-console.log(rabbit.alive);
-rabbit.eat();
-rabbit.sleep();
 rabbit.run();
 
-console.log(fish.alive);
-fish.eat();
-fish.sleep();
-fish.swim();
-
-console.log(hawk.alive);
-hawk.eat();
-hawk.sleep();
-hawk.fly();
 
 
 
