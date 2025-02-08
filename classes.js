8d8bfbc15ee4a7d45d406523306ea8c2fab716c6
Sandesh Paudel <sandeshpaudel0618@gmail.com>
2025-02-08 12:32:43 +0545

    Inheritance In Classes completed

--- classes.js
+++ classes.js
@@ -120,3 +120,67 @@
 
 
 
+///Inheritance :->   allows a new class to inherit properties and methods 
+//                   from an existing class (parent -> child) 
+//                   helps with code reusabliliy
+
+
+class Animal{
+    alive=true;
+
+    eat(){
+       console.log(`This ${this.name} is eating`); 
+    }
+
+    sleep(){
+        console.log(`This ${this.name} is sleeping`); 
+     }
+}
+
+
+class Rabbit extends Animal{
+     name="Rabbit"; 
+
+     //can have individual functions too
+     run(){
+        console.log(`This ${this.name} is running`);
+     }
+}
+
+class Fish extends Animal{
+     name="Fish";
+     swim(){
+        console.log(`This ${this.name} is swimming`);
+     }
+}
+
+class Hawk extends Animal{
+     name="Hawk"; 
+
+     fly(){
+        console.log(`This ${this.name} is flying`);
+     }
+}
+
+const rabbit= new Rabbit();
+const fish= new Fish();
+const hawk= new Hawk();
+
+rabbit.alive=false;
+console.log(rabbit.alive);
+rabbit.eat();
+rabbit.sleep();
+rabbit.run();
+
+console.log(fish.alive);
+fish.eat();
+fish.sleep();
+fish.swim();
+
+console.log(hawk.alive);
+hawk.eat();
+hawk.sleep();
+hawk.fly();
+
+
+
