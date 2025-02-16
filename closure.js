02673800cb0904de59a31b52c388202c15e459b4
Sandesh Paudel <sandeshpaudel0618@gmail.com>
2025-02-16 22:13:29 +0545

    setTImeout  and clearTimeout Function Completed

--- closure.js
+++ closure.js
@@ -46,34 +46,78 @@
 
 // console.log(`The current count is ${counter.getCount()}`);
 
-function createGame(){
+// function createGame(){
 
-    let score=0;
+//     let score=0;
 
-    function increaseScore(points){
-        score+=points;
-        console.log(`+${points}pts`);
-    }
+//     function increaseScore(points){
+//         score+=points;
+//         console.log(`+${points}pts`);
+//     }
 
-    function decreaeScore(points){
-        score -=points;
-        console.log(`-${points}pts`);
-    }
+//     function decreaeScore(points){
+//         score -=points;
+//         console.log(`-${points}pts`);
+//     }
 
-    function getScore(points){
-    return score;
-    }
+//     function getScore(points){
+//     return score;
+//     }
 
-    return{increaseScore,decreaeScore,getScore}
-}
+//     return{increaseScore,decreaeScore,getScore}
+// }
 
 // increaseScore(5);
 // increaseScore(10);
 // decreaeScore(10);
 // console.log(getScore());
 
-const game= createGame();
-game.increaseScore(5);
-game.increaseScore(10);
-game.decreaeScore(10);
-console.log(game.getScore());
+// const game= createGame();
+// game.increaseScore(5);
+// game.increaseScore(10);
+// game.decreaeScore(10);
+// console.log(game.getScore());
+
+
+
+
+
+
+
+
+
+
+
+
+// ----------------------------setTimeout()
+
+
+// function sayHello(){
+//     window.alert("Hello");
+
+// }
+
+// setTimeout(sayHello,3000);
+
+// setTimeout(function(){window.alert("Hello")},3000);
+
+// setTimeout(()=>window.alert("Hello"),3000);
+
+// const timeoutId=setTimeout(()=>window.alert("Hello"),3000);
+// clearTimeout(timeoutId);
+
+
+let timeoutId;
+function startTimer(){
+    timeoutId=setTimeout(()=>window.alert("Hello"),3000);
+    console.log("Started");
+}
+
+function clearTimer(){
+    clearTimeout(timeoutId,window.alert("Cleard Timeout"));
+    console.log("Cleared");
+
+
+}
+
+
