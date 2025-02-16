d6f989e99aba77b38a5a95456e8a0f67a83bf985
Sandesh Paudel <sandeshpaudel0618@gmail.com>
2025-02-16 22:00:02 +0545

    Closures completed

--- closure.js
+++ closure.js
@@ -6,12 +6,74 @@
 
 
 
-function outer()
-{
-    let message="Hello";
-    function inner(){
-        console.log(message);
+// function outer()
+// {
+//     let message="Hello";
+
+//     function inner(){
+//         console.log(message);
+//     }
+
+//     inner();
+// }
+// outer();
+
+
+
+
+
+// function creatCounter(){
+
+//     let count=0;
+//     function increment(){
+//         count++;
+//         console.log(`Count increased to ${count} `);
+//     }
+
+//     function getCount(){
+//         return count;
+//     }
+
+//     return {increment,getCount};
+// }
+
+// const counter=creatCounter();
+
+// counter.increment();
+// counter.increment();
+// counter.increment();
+
+
+// console.log(`The current count is ${counter.getCount()}`);
+
+function createGame(){
+
+    let score=0;
+
+    function increaseScore(points){
+        score+=points;
+        console.log(`+${points}pts`);
     }
+
+    function decreaeScore(points){
+        score -=points;
+        console.log(`-${points}pts`);
+    }
+
+    function getScore(points){
+    return score;
+    }
+
+    return{increaseScore,decreaeScore,getScore}
 }
 
-outer();
+// increaseScore(5);
+// increaseScore(10);
+// decreaeScore(10);
+// console.log(getScore());
+
+const game= createGame();
+game.increaseScore(5);
+game.increaseScore(10);
+game.decreaeScore(10);
+console.log(game.getScore());
