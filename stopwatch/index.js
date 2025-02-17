ffc43e9c97331787a5260d25695609c9d4e7b470
Sandesh Paudel <sandeshpaudel0618@gmail.com>
2025-02-17 22:08:25 +0545

    Stopwatch Completed

--- stopwatch/index.js
+++ stopwatch/index.js
@@ -13,25 +13,36 @@ function start() {
   }
 }
 
-function stop() {}
+function stop() {
+  if (isRunning) {
+    clearInterval(timer);
+    elapseTime = Date.now() - startTime;
+    isRunning = false;
+  }
+}
 
-function reset() {}
+function reset() {
+clearInterval(timer);
+   
+   startTime = 0;
+   elapseTime = 0;
+   isRunning = false;
+   display.textContent="00:00:00:00";
+}
 
 function update() {
   const currentTime = Date.now();
   elapseTime = currentTime - startTime;
 
-  let hours = Math.flooor(elapseTime / (1000 * 60 * 60));
-  let minutes = Math.flooor(elapseTime / (1000 * 60 % 60));
-  let seconds = Math.flooor((elapseTime / 1000) % 60);
+  let hours = Math.floor(elapseTime / (1000 * 60 * 60));
+  let minutes = Math.floor((elapseTime / (1000 * 60)) % 60);
+  let seconds = Math.floor((elapseTime / 1000) % 60);
   let milliseconds = Math.floor((elapseTime % 1000) / 10);
 
-
-  hours=String(hours).padStart(2,"0");
-  minutes=String(minutes).padStart(2,"0");
-  seconds=String(seconds).padStart(2,"0");
-  milliseconds=String(milliseconds).padStart(2,"0");
-
+  hours = String(hours).padStart(2, "0");
+  minutes = String(minutes).padStart(2, "0");
+  seconds = String(seconds).padStart(2, "0");
+  milliseconds = String(milliseconds).padStart(2, "0");
 
   display.textContent = `${hours}:${minutes}:${seconds}:${milliseconds}`;
 }
