663eeb913aff496a1d79f700ff267037a9c911ee
Sandesh Paudel <sandeshpaudel0618@gmail.com>
2025-02-18 23:56:12 +0545

    DOM Started

---
+++ dom.js
@@ -0,0 +1,7 @@
+//DOM - DOCUMENT OBJECT MODEL
+//      Obejct that represents the page you see in the browser
+//      and provides you with an API to interest with it.
+//      Web browser constructs the DOM when it loads an HTML file
+//      and structures all the elements in a tree-like representation
+//      JS can access the DOM to dynamically
+//      change the content, structure and style of the web page.
