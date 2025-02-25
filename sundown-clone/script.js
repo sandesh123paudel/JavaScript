35db2143ed9462891e0c5f66951a880e1c417f74
Sandesh Paudel <sandeshpaudel0618@gmail.com>
2025-02-25 23:35:26 +0545

    Text Animation Completed

--- sundown-clone/script.js
+++ sundown-clone/script.js
@@ -1,6 +1,4 @@
-import LocomotiveScroll from 'locomotive-scroll';
-
 const scroll = new LocomotiveScroll({
-    el: document.querySelector('[data-scroll-container]'),
+    el: document.querySelector('#main'),
     smooth: true
 });
