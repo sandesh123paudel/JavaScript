20bbddf1c861526939b8d52dc3bced7a0cd041c9
Sandesh Paudel <sandeshpaudel0618@gmail.com>
2025-02-28 20:34:21 +0545

    ..

--- sundown-clone/script.js
+++ sundown-clone/script.js
@@ -2,3 +2,17 @@ const scroll = new LocomotiveScroll({
     el: document.querySelector('#main'),
     smooth: true
 });
+
+const scroll = new LocomotiveScroll({
+    el: document.querySelector('#main'),
+    smooth: true
+});
+
+const scroll = new LocomotiveScroll({
+    el: document.querySelector('#main'),
+    smooth: true
+});
+const scroll = new LocomotiveScroll({
+    el: document.querySelector('#main'),
+    smooth: true
+});
