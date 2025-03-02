23e29e4ff93bcefac4493e988937171c08447df4
Sandesh Paudel <sandeshpaudel0618@gmail.com>
2025-03-02 12:31:41 +0545

    Fixed Image Added

--- sundown-clone/script.js
+++ sundown-clone/script.js
@@ -3,3 +3,18 @@ const scroll = new LocomotiveScroll({
     smooth: true
 });
 
+
+// let a= document.querySelector("#elem1");
+// let image=a.getAttribute("data-image");
+
+let elemC=document.querySelector("#elem-container");
+let fixedImage=document.querySelector("#fixed-image")
+elemC.addEventListener("mouseenter",function(){
+    fixedImage.getAttribute("display")
+    fixedImage.style.display="block"; 
+});
+
+elemC.addEventListener("mouseleave",function(){
+    fixedImage.getAttribute("display")
+    fixedImage.style.display="none"; 
+});
